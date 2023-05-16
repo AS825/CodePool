a905145423be0e351b65efd87c4b09a8e653329d
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-16 17:21:38 +0200

    Create navbar

--- frontend/src/Pages/Navbar/Navbar.jsx
+++ frontend/src/Pages/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
+import "./Navbar.css";
+import Logo from "../../assets/codeCool_Logo.png";
+import { NavLink, Outlet } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav>
+        <div className="logo-container">
+          <img src={Logo} alt="codecool" className="CC-Logo" />
+          <NavLink to="#" className="Logo-name">
+            {" "}
+            CodePool
+          </NavLink>
+        </div>
+        <ul className="navbar">
+          <li></li>
+          <li>
+            <NavLink to="#">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/students">Students</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Sign In</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Navbar;
