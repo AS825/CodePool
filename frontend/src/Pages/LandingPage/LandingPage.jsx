1b8012406d8988b1792393ade780bbd64e57f67b
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-05-16 15:46:21 +0200

    Add functionality to fetch and display test data

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,19 @@
+import {useState, useEffect} from 'react';
+import { fetchMessage } from '../../Utils/fetchMethods';
+
 const LandingPage = () => {
+  const [message, setMessage] = useState(); 
+  
+  useEffect( () => {
+    fetchMessage().then(data => setMessage(data)); 
+  }, [])
+
   return (
-    <div>LandingPage</div>
+    <>
+      <div>LandingPage</div>
+      <div>Message: {message}</div>
+    </>
   )
 }
 
-export default LandingPage
+export default LandingPage;
