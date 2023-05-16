338b25f0ae7b94e8d604f19b8a02a2a5ce420004
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-16 17:19:16 +0200

    Add child element to render Students component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Navbar from './Pages/Navbar/Navbar';
-import ErrorPage from './Pages/ErrorPage/ErrorPage';
-import LandingPage from './Pages/LandingPage/LandingPage';
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./Pages/Navbar/Navbar";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import LandingPage from "./Pages/LandingPage/LandingPage";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Students from "./Pages/Students/Students";
 
 const router = createBrowserRouter([
   {
-    path: "/", 
-    element: <Navbar />, 
-    errorElement: <ErrorPage />, 
+    path: "/",
+    element: <Navbar />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
-        element: <LandingPage />
+        element: <LandingPage />,
+      },
+      {
+        path: "/students",
+        element: <Students />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider  router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
