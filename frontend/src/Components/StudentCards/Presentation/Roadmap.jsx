d6e73e39859206b18dd22f5e834c35cc6064e17a
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-05-17 15:20:39 +0200

    Add React component Roadmap

---
+++ frontend/src/Components/StudentCards/Presentation/Roadmap.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Roadmap = () => {
+  return (
+    <div className="image-div">
+        <img src={"/public/Roadmap.png"} alt="" className="image" />
+    </div>
+  )
+}
+
+export default Roadmap
