61607aaad22139cd0d45c3d38b40d2b4689ac572
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-17 15:04:49 +0200

    Create class to store students

---
+++ backend/src/main/java/com/codecool/backend/model/StudentStorage.java
@@ -0,0 +1,13 @@
+package com.codecool.backend.model;
+
+import com.codecool.backend.service.StudentCreator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudentStorage {
+    public Set<Student> readAll() {
+        StudentCreator studentCreator = new StudentCreator();
+        return studentCreator.createStudents();
+    }
+}
