d1e2af88835c6ffc64e0f94bae02ddae3b3e5d84
Alen Sacipi <alensacipi2@gmail.com>
2023-05-17 16:43:10 +0200

    Refactor fetchmethods

--- frontend/src/Utils/fetchMethods.js
+++ frontend/src/Utils/fetchMethods.js
@@ -1,14 +1,25 @@
 import axios from "axios";
 
-const path = "http://localhost:8080/"; 
+const path = "http://localhost:8080/";
+
+const getRequest = async (pathEndpoint) => {
+  try {
+    const response = await axios.get(path + pathEndpoint);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching message: ", error);
+    throw error;
+  }
+};
 
 export const fetchMessage = async () => {
-    try {
-        const response = await axios.get(path); 
-        return response.data; 
-    }
-    catch (error) {
-        console.error("Error fetching message: ", error); 
-        throw error; 
-    }
-}
+  const request = await getRequest("");
+  return request;
+};
+
+export const fetchStudents = async () => {
+    const request = await getRequest("students");
+    return request;
+  };
+
+
