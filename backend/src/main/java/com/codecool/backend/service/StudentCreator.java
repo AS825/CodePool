5a9e84066228f5ee9297f4a426e904c4c9b6a95b
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-17 16:36:46 +0200

    Change content of tasks

--- backend/src/main/java/com/codecool/backend/service/StudentCreator.java
+++ backend/src/main/java/com/codecool/backend/service/StudentCreator.java
@@ -8,14 +8,15 @@ import java.util.Set;
 
 public class StudentCreator {
 
-    private String title = "Full Stack Developer";
+    private String title = "Full Stack Developer student";
     private Set<Student> students= new HashSet<>();
     private String imgsrc;
     public Set<Student> createStudents() {
 
-        students.addAll(List.of(new Student(1,"Alen Sacipi",title, List.of("Test", "test", "test"),"alen"),
-                new Student(2,"Bernadette Christiner",title, List.of("Test", "test", "test"),"berni"),
-                new Student(3,"Peter Niederwieser",title, List.of("Test", "test", "test"),"peter")));
+        students.addAll(List.of(new Student(1,"Alen Sacipi",title, List.of("Frontend", "Backend", "Project Management"),"alen"),
+                new Student(2,"Bernadette Christiner",title, List.of("Frontend", "Backend", "Project Management"),"berni"),
+                new Student(3,"Peter Niederwieser",title, List.of("Frontend", "Backend", "Project Management"),"peter")));
         return students;
+
     }
 }
