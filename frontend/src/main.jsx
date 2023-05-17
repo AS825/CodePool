7a73bc0f39b268603819785388537a5609761be7
Alen Sacipi <alensacipi2@gmail.com>
2023-05-17 16:38:06 +0200

    Add Presentation in Navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import LandingPage from "./Pages/LandingPage/LandingPage";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Students from "./Pages/Students/Students";
+import Presentation from "./Components/StudentCards/Presentation/Presentation";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: "/students",
         element: <Students />,
+      }, 
+      {
+        path: "/presentation",
+        element: <Presentation />,
       },
     ],
   },
