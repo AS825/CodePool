6dcd239b841150ee2333ce507a91b26c91d92273
Alen Sacipi <alensacipi2@gmail.com>
2023-05-17 17:23:59 +0200

    Add Prensentation Intro

--- frontend/src/Components/StudentCards/Presentation/Presentation.jsx
+++ frontend/src/Components/StudentCards/Presentation/Presentation.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Intro from "./Intro"
 import ProjectGoals from "./ProjectGoals"
 import Roadmap from "./Roadmap"
 
 function Presentation() {
   return (
     <div>
+      <Intro />
       <ProjectGoals />
       <Roadmap />
     </div>
