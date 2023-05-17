0d5e5e7183957813b34eb1834eb20fd03326302a
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-17 15:03:26 +0200

    Create getRequest to display students

--- backend/src/main/java/com/codecool/backend/configuration/BeansConfiguration.java
+++ backend/src/main/java/com/codecool/backend/configuration/BeansConfiguration.java
@@ -2,14 +2,20 @@ package com.codecool.backend.configuration;
 
 
 import com.codecool.backend.model.HelloWorld;
+import com.codecool.backend.model.StudentStorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.HashSet;
 
 @Configuration
 public class BeansConfiguration {
 
     @Bean
     HelloWorld helloWorld() {
-      return new HelloWorld();
+        return new HelloWorld();
+    }
+   @Bean
+    StudentStorage studentStorage() {
+      return new StudentStorage();
     }
 }
