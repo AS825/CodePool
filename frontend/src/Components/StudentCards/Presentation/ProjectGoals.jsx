138729d31cde2f7ea74144d84441d5b8a27e4da8
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-05-17 15:19:59 +0200

    Add React component ProjectGoals

---
+++ frontend/src/Components/StudentCards/Presentation/ProjectGoals.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const ProjectGoals = () => {
+  return (
+    <div className="image-div">
+        <img src={"/public/ProjectGoals.png"} alt="" className="image" />
+    </div>
+  )
+}
+
+export default ProjectGoals
