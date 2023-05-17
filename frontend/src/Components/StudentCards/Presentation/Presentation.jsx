c8ab83f0e1853b024c88f73ba35a1d5c42f49481
Alen Sacipi <alensacipi2@gmail.com>
2023-05-17 16:39:05 +0200

    Add Presentation Component

---
+++ frontend/src/Components/StudentCards/Presentation/Presentation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectGoals from "./ProjectGoals"
+import Roadmap from "./Roadmap"
+
+function Presentation() {
+  return (
+    <div>
+      <ProjectGoals />
+      <Roadmap />
+    </div>
+  );
+}
+
+export default Presentation;
