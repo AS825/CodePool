29af4a38a4686f3b6331ee0c4f01d727a11c578f
Alen Sacipi <alensacipi2@gmail.com>
2023-05-17 16:39:58 +0200

    Add Student.jsx

--- frontend/src/Components/StudentCards/StudentCard/StudentCard.jsx
+++ frontend/src/Components/StudentCards/StudentCard/StudentCard.jsx
@@ -1,16 +1,33 @@
+/* eslint-disable react/prop-types */
+import "./StudentCard.css"
+// import alen from "../../../assets/alen.jpeg"
+// import berni from "../../../assets/berni.png"
+
 const StudentCard = ({student}) => {
+  console.log(student.name);
   return (
-    <div className="student-card">
-        <div className="header-section">
-            <img src={student.src} alt="" className="student-image"/> 
-        </div>
-        <div className="body-section">
-            <div className="name"> {student.name} </div>
-            <div className="tasks"> {student.tasks}</div>
-        </div>
-        <div></div>
+    <div className="card card-shadow">
+    <div className="card-background">
+    </div>
+    <div >
+      <img className="img-radius" src={`${student.imgsrc +".jpeg"}`} alt="" />
+    </div>
+    <div className="card-details">
+    <h2>{student.name}</h2>
+    <p>{student.title}</p>
+    <p>TASKS:</p>
+    <div className="task-container">
+      <p>{student.tasks[0]}</p>
+      <p>{student.tasks[1]}</p>
+      <p>{student.tasks[2]}</p>
+    </div>
 
     </div>
+    <div className="card-footer">
+      <button className="btn">Details</button>
+      <button className="btn btn-outline">Contact Seller</button>
+    </div>
+  </div>
   )
 }
 
