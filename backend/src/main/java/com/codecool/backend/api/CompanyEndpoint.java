3800b4346d8e9cddad5c713bead4398755deb987
Alen Sacipi <alensacipi2@gmail.com>
2023-05-23 12:18:34 +0200

    Add CompanyEndPoint Class

---
+++ backend/src/main/java/com/codecool/backend/api/CompanyEndpoint.java
@@ -0,0 +1,34 @@
+package com.codecool.backend.api;
+
+import com.codecool.backend.model.entity.Company;
+import com.codecool.backend.service.CompanyService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("companies")
+@CrossOrigin(origins = "*")
+public class CompanyEndpoint {
+    private final CompanyService companyService;
+
+    public CompanyEndpoint(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    List<Company> getAll() {
+        return companyService.getAllCompanies();
+    }
+
+    @PostMapping
+    Company post(@RequestBody Company company) {
+        return companyService.saveCompany(company);
+    }
+
+    @DeleteMapping("{id}")
+    void delete(@PathVariable long id) {
+        companyService.deleteCompany(id);
+    }
+
+}
---
+++ backend/src/main/java/com/codecool/backend/model/entity/Company.java
@@ -0,0 +1,44 @@
+package com.codecool.backend.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "companies")
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String contactMail;
+
+    public Company(String name, String contactMail) {
+        this.name = name;
+        this.contactMail = contactMail;
+    }
+    public Company() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactMail() {
+        return contactMail;
+    }
+
+    public void setContactMail(String contactMail) {
+        this.contactMail = contactMail;
+    }
+}
