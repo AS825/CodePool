e10d747c38263fde86fd65e45253bd56a73d7426
Alen Sacipi <alensacipi2@gmail.com>
2023-05-23 10:27:14 +0200

    Add StudentRepository Interface

---
+++ backend/src/main/java/com/codecool/backend/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.backend.repository;
+
+import com.codecool.backend.model.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository <Student, Long> {
+}
