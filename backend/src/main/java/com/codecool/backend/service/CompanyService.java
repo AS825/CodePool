fbe06e085ea6e16b1aa8935e00930738fe4ff229
Alen Sacipi <alensacipi2@gmail.com>
2023-05-23 12:19:26 +0200

    Add CompanyService Class

---
+++ backend/src/main/java/com/codecool/backend/service/CompanyService.java
@@ -0,0 +1,26 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.model.entity.Company;
+import com.codecool.backend.repository.CompanyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+    public Company saveCompany(Company company) {
+        return companyRepository.save(company);
+    }
+    public void deleteCompany(long id) {
+        companyRepository.deleteById(id);
+    }
+
+}
