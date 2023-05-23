fb9598e2fa54786850d8cea1f82aa4e25faccec2
Alen Sacipi <alensacipi2@gmail.com>
2023-05-23 12:14:55 +0200

    Format repository Clean Code

--- backend/src/main/java/com/codecool/backend/repository/StudentRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.codecool.backend.model.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository <Student, Long> {
+
 }
