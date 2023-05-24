36a41d52ee8f1edb904c622e3f1bff84c8b53e01
Alen Sacipi <alensacipi2@gmail.com>
2023-05-24 19:41:15 +0200

    Add CompanyTransformer class

---
+++ backend/src/main/java/com/codecool/backend/reader/CompanyTransformer.java
@@ -0,0 +1,16 @@
+package com.codecool.backend.reader;
+
+
+import com.codecool.backend.model.entity.Company;
+
+public class CompanyTransformer implements DataTransformer<Company> {
+    @Override
+    public Company transform(String line) {
+        String[] columns = line.split(",");
+        String companyName = columns[0];
+        String email = columns[1];
+
+        return new Company(companyName,email);
+
+    }
+}
