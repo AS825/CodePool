a5f4cd3791225e95f8b0576a2160c4df007cbdf1
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-05-24 16:34:27 +0200

    Add tests for methods getAllStudents() and getStudentById() in class StudentService

---
+++ backend/src/test/java/com/codecool/backend/service/student/StudentServiceTest.java
@@ -0,0 +1,60 @@
+package com.codecool.backend.service.student;
+
+import com.codecool.backend.model.entity.Student;
+import com.codecool.backend.repository.StudentRepository;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class StudentServiceTest {
+    StudentRepository studentRepository = mock(StudentRepository.class);
+    StudentService studentService = new StudentService(studentRepository);
+
+    @Test
+    void getAllStudents() {
+        studentService.getAllStudents();
+
+        verify(studentRepository).findAll();
+    }
+
+    @Test
+    void getStudentById_StudentIsFound() throws StudentNotFoundException {
+        long id = 1;
+        Student student = new Student("TestStudent", "TestDescription", "TestProject", "TestImgSrc");
+        when(studentRepository.findById(id)).thenReturn(Optional.of(student));
+
+        Student result = studentService.getStudentById(id);
+
+        verify(studentRepository).findById(id);
+        assertEquals(student, result);
+    }
+
+    @Test
+    void getStudentById_StudentIsNotFound() {
+        long notExistingId = 1;
+        when(studentRepository.findById(notExistingId)).thenReturn(Optional.empty());
+
+        assertThrows(StudentNotFoundException.class, () -> studentService.getStudentById(notExistingId));
+    }
+
+    @Test
+    void saveStudent() {
+    }
+
+    @Test
+    void updateStudentById() {
+    }
+
+    @Test
+    void updateStudent1() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
