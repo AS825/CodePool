c7ce2772c9c838cce72f2f5adc15bcc8c8a6ae82
Alen Sacipi <alensacipi2@gmail.com>
2023-05-24 19:44:24 +0200

    Add getCompanyById method in CompanyService

--- backend/src/main/java/com/codecool/backend/service/company/CompanyService.java
+++ backend/src/main/java/com/codecool/backend/service/company/CompanyService.java
@@ -3,7 +3,6 @@ package com.codecool.backend.service.company;
 import com.codecool.backend.model.entity.Company;
 import com.codecool.backend.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
@@ -13,14 +12,23 @@ public class CompanyService {
     public CompanyService(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
+
+    public Company getCompanyById(long id) throws CompanytNotFoundException {
+        return companyRepository.findById(id)
+                .orElseThrow(CompanytNotFoundException::new);
+    }
+
     public List<Company> getAllCompanies() {
         return companyRepository.findAll();
     }
+
     public Company saveCompany(Company company) {
         return companyRepository.save(company);
     }
+
     public void deleteCompany(long id) {
         companyRepository.deleteById(id);
     }
+
 
 }
