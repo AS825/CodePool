4fbc2b95126827b497a7d349227f68212ff78c7b
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-05-24 08:03:52 +0200

    Add class CompanyEndpointTest

---
+++ backend/src/test/java/com/codecool/backend/api/CompanyEndpointTest.java
@@ -0,0 +1,20 @@
+package com.codecool.backend.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyEndpointTest {
+
+    @Test
+    void getAllCompanies() {
+    }
+
+    @Test
+    void saveCompany() {
+    }
+
+    @Test
+    void deleteCompany() {
+    }
+}
