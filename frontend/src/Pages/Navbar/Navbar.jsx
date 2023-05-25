d0ef99263e72db7419182987c502aa65bc0058f6
Alen Sacipi <alensacipi2@gmail.com>
2023-05-26 01:28:41 +0200

    Add new Links to Navbar

--- frontend/src/Pages/Navbar/Navbar.jsx
+++ frontend/src/Pages/Navbar/Navbar.jsx
@@ -1,28 +1,38 @@
 import "./Navbar.css";
 import Logo from "../../assets/codeCool_Logo.png";
 import { NavLink, Outlet } from "react-router-dom";
-
+import { UilUser } from "@iconscout/react-unicons";
 const Navbar = () => {
   return (
     <div>
       <nav>
         <div className="logo-container">
           <img src={Logo} alt="codecool" className="CC-Logo" />
-          <NavLink to="/" className="Logo-name">
+          <NavLink to="/" className="Logo-name hover-link">
             {" "}
             CodePool
           </NavLink>
         </div>
         <ul className="navbar">
-          <li></li>
           <li>
-            <NavLink to="/">Projects</NavLink>
+            <NavLink className="hover-link" to="/">
+              Projects
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/students">Students</NavLink>
+            <NavLink className="hover-link" to="/students">
+              Students
+            </NavLink>
           </li>
           <li>
-            <NavLink to="#">Sign In</NavLink>
+            <NavLink className="hover-link" to="/contact">
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="hover-link" to="#">
+              <UilUser /> Sign In{" "}
+            </NavLink>
           </li>
         </ul>
       </nav>
