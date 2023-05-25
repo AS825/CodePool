586bb58155bc8891c3db219a8f55f38e4837bd76
Alen Sacipi <alensacipi2@gmail.com>
2023-05-26 01:25:26 +0200

    Add path to StudenRepository

--- backend/src/main/java/com/codecool/backend/repository/StudentRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/StudentRepository.java
@@ -2,7 +2,12 @@ package com.codecool.backend.repository;
 
 import com.codecool.backend.model.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
+@RepositoryRestResource(path = "students")
 public interface StudentRepository extends JpaRepository <Student, Long> {
 
 }
