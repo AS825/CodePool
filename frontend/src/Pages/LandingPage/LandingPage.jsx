72890fb97395ef38ff33c08c9712e884a99667a9
Alen Sacipi <alensacipi2@gmail.com>
2023-05-26 01:28:30 +0200

    Remove samplecode

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,18 +1,9 @@
-import { useState, useEffect } from "react";
-import { fetchMessage } from "../../Utils/fetchMethods";
 import "./LandingPage.css";
 
 const LandingPage = () => {
-  const [message, setMessage] = useState();
-
-  useEffect(() => {
-    fetchMessage().then((data) => setMessage(data));
-  }, []);
-
   return (
     <>
       <div>LandingPage</div>
-      <div className="message">Message: {message}</div>
     </>
   );
 };
