3b8c939f972938c525a1436560ed356171e78684
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 20:02:20 +0200

    Create graduates class

--- backend/src/main/java/com/codecool/backend/model/entity/Graduate.java
+++ backend/src/main/java/com/codecool/backend/model/entity/Graduate.java
@@ -3,27 +3,25 @@ package com.codecool.backend.model.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="graduates")
-public class Graduates {
+@Table(name = "graduates")
+public class Graduate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private final String firstName;
-    private final String lastName;
-    private final String employer;
-    private final String position;
-    private final String imgSrc;
+    private String firstName;
+    private String lastName;
+    private String employer;
+    private String position;
+    @Column(length = 1000000)
+    //@Column(length = 1000000)
+    private String imgSrc;
 
-    public Graduates(String firstName, String lastName, String employer, String position, String imgSrc) {
+    public Graduate(String firstName, String lastName, String employer, String position, String imgSrc) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.employer = employer;
         this.position = position;
         this.imgSrc = imgSrc;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public long getId() {
@@ -50,7 +48,7 @@ public class Graduates {
         return imgSrc;
     }
 
-    public Graduates() {
+    public Graduate() {
 
     }
 }
