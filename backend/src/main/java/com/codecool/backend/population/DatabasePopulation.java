88fd66ab1628c908fc552f997f05e40f75744439
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 20:02:05 +0200

    Extend class with data of graduates

--- backend/src/main/java/com/codecool/backend/population/DatabasePopulation.java
+++ backend/src/main/java/com/codecool/backend/population/DatabasePopulation.java
@@ -1,26 +1,41 @@
 package com.codecool.backend.population;
 
 import com.codecool.backend.model.entity.Company;
+import com.codecool.backend.model.entity.Graduate;
 import com.codecool.backend.model.entity.Student;
 import com.codecool.backend.repository.CompanyRepository;
+import com.codecool.backend.repository.WelcomePageRepository;
 import com.codecool.backend.repository.StudentRepository;
+import com.codecool.backend.service.converter.Base64Converter;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
 public class DatabasePopulation {
 
     @Bean
-    ApplicationRunner populate(StudentRepository studentRepository, CompanyRepository companyRepository) {
+    ApplicationRunner populate(StudentRepository studentRepository, CompanyRepository companyRepository, WelcomePageRepository graduateRepository) {
         return args -> {
             List<Student> students = createStudents();
             studentRepository.saveAll(students);
             List<Company> companies = createCompanies();
             companyRepository.saveAll(companies);
+            List<Graduate> graduates = createGraduates();
+            graduateRepository.saveAll(graduates);
+
         };
+    }
+
+    private List<Graduate> createGraduates() throws IOException {
+        Base64Converter base64Converter = new Base64Converter();
+        return List.of(
+                new Graduate("Mark", "Hartleib", "Schrack Technik", "Junior ERP Developer", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Mark.png")),
+                new Graduate("Roman", "Fann", "Smatrics", "Junior Developer", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Roman.png"))
+        );
     }
 
     private List<Student> createStudents() {
