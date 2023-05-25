914a2ff24a52071e555cc335a347c5bbd5490212
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 20:14:00 +0200

    Create headline for footer

--- frontend/src/Pages/LandingPage/Footer/Footer.jsx
+++ frontend/src/Pages/LandingPage/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Contact from "./Contact";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>
+    <div className="footer">
+      <h2>Work with us</h2>
       <Contact />
     </div>
   );
