19bda579f6d3561aaf8a10450c6006b2a563eceb
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 10:20:28 +0200

    Create header component

---
+++ frontend/src/Pages/LandingPage/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+import Video from "./YoutubeEmb";
+
+function Header() {
+  return (
+    <div className="headLine">
+      <h1>Invest in your company with our qualified developers</h1>
+      <Video />
+    </div>
+  );
+}
+
+export default Header;
