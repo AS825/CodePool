4189551d8d2aadcd036e7a74dd61f515befc1c38
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 20:03:32 +0200

    Create a WelcomPageRepository

--- backend/src/main/java/com/codecool/backend/repository/WelcomePageRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/WelcomePageRepository.java
@@ -3,5 +3,5 @@ package com.codecool.backend.repository;
 import com.codecool.backend.model.entity.Graduate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GraduateRepository extends JpaRepository<Graduate,Long> {
+public interface WelcomePageRepository extends JpaRepository<Graduate,Long> {
 }
