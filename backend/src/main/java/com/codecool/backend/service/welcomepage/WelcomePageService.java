4379872a2b08eb4401728be66ee7c5d2994ea961
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 20:03:51 +0200

    Create a service class for WelcomePage requests

--- backend/src/main/java/com/codecool/backend/service/welcomepage/WelcomePageService.java
+++ backend/src/main/java/com/codecool/backend/service/welcomepage/WelcomePageService.java
@@ -1,2 +1,20 @@
-package com.codecool.backend.service.welcomepage;public class WelcomePageService {
+package com.codecool.backend.service.welcomepage;
+
+import com.codecool.backend.model.entity.Graduate;
+import com.codecool.backend.repository.WelcomePageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WelcomePageService {
+    private final WelcomePageRepository welcomePageRepository;
+
+    public WelcomePageService(WelcomePageRepository welcomePageRepository) {
+        this.welcomePageRepository = welcomePageRepository;
+    }
+
+    public List<Graduate> getGraduatesForSlideshow() {
+        return welcomePageRepository.findAll();
+    }
 }
