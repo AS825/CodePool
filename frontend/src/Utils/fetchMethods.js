6e7638104b837d148ae357e0176962aa185278d5
Alen Sacipi <alensacipi2@gmail.com>
2023-05-26 01:29:07 +0200

    Add new fetchMethods

--- frontend/src/Utils/fetchMethods.js
+++ frontend/src/Utils/fetchMethods.js
@@ -1,14 +1,43 @@
 import axios from "axios";
 
-const path = "http://localhost:8080/"; 
+const path = "http://localhost:8090/";
 
 export const fetchMessage = async () => {
+  try {
+    const response = await axios.get(path);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching message: ", error);
+    throw error;
+  }
+};
+
+export const fetchStudents = async () => {
+  try {
+    const response = await axios.get(path + "api/students");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching message: ", error);
+    throw error;
+  }
+};
+
+export const fetchNextPageOfStudents = async (page) => {
     try {
-        const response = await axios.get(path); 
-        return response.data; 
+        const response = await axios.get(path + `api/students?page=${page}`);
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching message: ", error);
+        throw error;
+      }
+  };
+
+  export const fetchStudent = async (id) => {
+    try {
+      const response = await axios.get(path + `api/students/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching message: ", error);
+      throw error;
     }
-    catch (error) {
-        console.error("Error fetching message: ", error); 
-        throw error; 
-    }
-}
+  };
