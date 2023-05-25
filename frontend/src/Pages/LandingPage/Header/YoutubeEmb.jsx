cf84787dbbab7f3d83000079ef750da6bcb5c884
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 10:22:32 +0200

    Add frameborder attribute

--- frontend/src/Pages/LandingPage/Header/YoutubeEmb.jsx
+++ frontend/src/Pages/LandingPage/Header/YoutubeEmb.jsx
@@ -9,6 +9,7 @@ function YoutubeEmb() {
         height="315"
         src="https://www.youtube.com/embed/qyV_A9wRHO8"
         title="YouTube video player"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
