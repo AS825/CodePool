3eefb07861fea9508c1ea25fe3916f00f555351d
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-25 20:03:06 +0200

    Add request method to get all graduates

--- backend/src/main/java/com/codecool/backend/api/WelcomePageEndpoint.java
+++ backend/src/main/java/com/codecool/backend/api/WelcomePageEndpoint.java
@@ -1,2 +1,28 @@
-package com.codecool.backend.api;public class WelcomepageEndpoint {
+package com.codecool.backend.api;
+
+import com.codecool.backend.model.entity.Graduate;
+import com.codecool.backend.service.welcomepage.WelcomePageService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/graduates")
+@CrossOrigin(origins = "*")
+public class WelcomePageEndpoint {
+
+    private final WelcomePageService welcomePageService;
+
+
+    public WelcomePageEndpoint(WelcomePageService welcomePageService) {
+        this.welcomePageService = welcomePageService;
+    }
+    @GetMapping
+    public List<Graduate> getGraduates() {
+        return welcomePageService.getGraduatesForSlideshow();
+    }
+
 }
