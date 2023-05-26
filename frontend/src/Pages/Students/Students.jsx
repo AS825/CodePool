73021798ee4664bae4f3753f91f433ca40bba27e
Alen Sacipi <alensacipi2@gmail.com>
2023-05-26 11:52:05 +0200

    Refactor StudentCss

--- frontend/src/Pages/Students/Students.jsx
+++ frontend/src/Pages/Students/Students.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from "react";
 import {
   fetchStudents,
-  fetchNextPageOfStudents,
+  // fetchNextPageOfStudents,
 } from "../../Utils/fetchMethods";
 import StudentsGrid from "../../Components/StudentsGrid";
 import "./Students.css";
+// import AOS from 'aos';
+// import "aos/dist/aos.css";
 
 function Students() {
   const [students, setStudents] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [loading, setLoading] = useState(false);
+  // const [currentPage, setCurrentPage] = useState(0);
+  // const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchStudents().then((data) => {
@@ -17,28 +19,28 @@ function Students() {
     });
   }, []);
 
-  const loadMoreStudents = () => {
-    if (loading) return;
-    setLoading(true);
-    fetchNextPageOfStudents(currentPage + 1).then((data) => {
-      const newStudents = data?._embedded.students;
-      setStudents((prevStudents) => [...prevStudents, ...newStudents]);
-      setCurrentPage(data?.page.number);
-      setLoading(false);
-    });
-  };
+  // const loadMoreStudents = () => {
+  //   if (loading) return;
+  //   setLoading(true);
+  //   fetchNextPageOfStudents(currentPage + 1).then((data) => {
+  //     const newStudents = data?._embedded.students;
+  //     setStudents((prevStudents) => [...prevStudents, ...newStudents]);
+  //     setCurrentPage(data?.page.number);
+  //     setLoading(false);
+  //   });
+  // };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  },);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // },);
 
-  const handleScroll = () => {
-    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-    if (scrollTop + clientHeight >= scrollHeight - 100) {
-      loadMoreStudents();
-    }
-  };
+  // const handleScroll = () => {
+  //   const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+  //   if (scrollTop + clientHeight >= scrollHeight - 100) {
+  //     loadMoreStudents();
+  //   }
+  // };
 
   return (
     <div className="students">
@@ -47,7 +49,7 @@ function Students() {
         <h3 className="sub-title">CodeCool Jobhunters</h3>
       </div>
       <StudentsGrid studentsList={students} />
-      {loading && <p>Loading more students...</p>}
+      {/* {loading && <p>Loading more students...</p>} */}
     </div>
   );
 }
