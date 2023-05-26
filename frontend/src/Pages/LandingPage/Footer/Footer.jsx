0a55ad11fe8450c16fb1cd99fa4ba32daf2f28a1
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-26 11:07:35 +0200

    Add conditional rendering

--- frontend/src/Pages/LandingPage/Footer/Footer.jsx
+++ frontend/src/Pages/LandingPage/Footer/Footer.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Contact from "./Contact";
 import "./Footer.css";
 
-function Footer() {
+function Footer({ officePersonal }) {
   return (
     <div className="footer">
       <h2>Work with us</h2>
-      <Contact />
+      {officePersonal == undefined ? (
+        console.log("loading ...")
+      ) : (
+        <Contact officePersonal={officePersonal} />
+      )}
     </div>
   );
 }
