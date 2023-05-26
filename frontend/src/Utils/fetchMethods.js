6cca6930761d660e6b7548e8d3d4da008d9595f4
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-26 11:08:13 +0200

    Include fetch to get data from office employees

--- frontend/src/Utils/fetchMethods.js
+++ frontend/src/Utils/fetchMethods.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
-const path = "http://localhost:8080/graduates";
+const pathGraduates = "http://localhost:8080/graduates";
+const pathOfficePersonal = "http://localhost:8080/office-personal";
 
-export const fetchData = async () => {
+export const fetchGraduates = async () => {
   try {
-    const response = await axios.get(path);
+    const response = await axios.get(pathGraduates);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching message: ", error);
+    throw error;
+  }
+};
+
+export const fetchOfficePersonal = async () => {
+  try {
+    const response = await axios.get(pathOfficePersonal);
     return response.data;
   } catch (error) {
     console.error("Error fetching message: ", error);
