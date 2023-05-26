dcde4fb5a5dfaae36bf76395e41e1005fba29b6f
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-26 23:41:25 +0200

    Add Column annotation

--- backend/src/main/java/com/codecool/backend/model/entity/OfficePersonal.java
+++ backend/src/main/java/com/codecool/backend/model/entity/OfficePersonal.java
@@ -3,25 +3,35 @@ package com.codecool.backend.model.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "officepersonal")
 public class OfficePersonal {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private long id;
-    private  String firstName;
-    private  String lastName;
-    private  String jobDescription;
-    private  String contactData;
+    private String firstName;
+    private String lastName;
+    private String jobDescription;
+    private String contactData;
+    @Column(length = 1000000)
+    private String imageSource;
 
-    public OfficePersonal(String firstName, String lastName, String jobDescription, String contactData) {
+    public OfficePersonal(String firstName, String lastName, String jobDescription, String contactData, String imageSource) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.jobDescription = jobDescription;
         this.contactData = contactData;
+        this.imageSource = imageSource;
     }
 
     public OfficePersonal() {
 
+    }
+
+    public String getImageSource() {
+        return imageSource;
+    }
+
+    public void setImageSource(String imageSource) {
+        this.imageSource = imageSource;
     }
 
     public void setId(long id) {
