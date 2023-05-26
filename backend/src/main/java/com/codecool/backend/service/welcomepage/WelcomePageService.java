3a96dd853830e506901c6be37a9202f99a2cb38d
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-26 11:10:45 +0200

    Add method to get all employees from office

--- backend/src/main/java/com/codecool/backend/service/welcomepage/WelcomePageService.java
+++ backend/src/main/java/com/codecool/backend/service/welcomepage/WelcomePageService.java
@@ -1,6 +1,8 @@
 package com.codecool.backend.service.welcomepage;
 
 import com.codecool.backend.model.entity.Graduate;
+import com.codecool.backend.model.entity.OfficePersonal;
+import com.codecool.backend.repository.OfficePersonalRepository;
 import com.codecool.backend.repository.WelcomePageRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +11,18 @@ import java.util.List;
 @Service
 public class WelcomePageService {
     private final WelcomePageRepository welcomePageRepository;
+    private final OfficePersonalRepository officePersonalRepository;
 
-    public WelcomePageService(WelcomePageRepository welcomePageRepository) {
+    public WelcomePageService(WelcomePageRepository welcomePageRepository, OfficePersonalRepository officePersonalRepository, OfficePersonalRepository officePersonalRepository1) {
         this.welcomePageRepository = welcomePageRepository;
+        this.officePersonalRepository = officePersonalRepository1;
     }
 
     public List<Graduate> getGraduatesForSlideshow() {
         return welcomePageRepository.findAll();
+    }
+
+    public List<OfficePersonal> getOfficePersonal() {
+        return officePersonalRepository.findAll();
     }
 }
