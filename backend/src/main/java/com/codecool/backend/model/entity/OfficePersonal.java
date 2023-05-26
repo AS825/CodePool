993a81f122f629d0880a264cda22c152b6b51afc
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-26 11:09:38 +0200

    Create entity for office personal

--- backend/src/main/java/com/codecool/backend/model/entity/OfficePersonal.java
+++ backend/src/main/java/com/codecool/backend/model/entity/OfficePersonal.java
@@ -1,2 +1,62 @@
-package com.codecool.backend.model.entity;public class OfficePersonal {
+package com.codecool.backend.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "officepersonal")
+public class OfficePersonal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private  String firstName;
+    private  String lastName;
+    private  String jobDescription;
+    private  String contactData;
+
+    public OfficePersonal(String firstName, String lastName, String jobDescription, String contactData) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobDescription = jobDescription;
+        this.contactData = contactData;
+    }
+
+    public OfficePersonal() {
+
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public void setContactData(String contactData) {
+        this.contactData = contactData;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public String getContactData() {
+        return contactData;
+    }
 }
