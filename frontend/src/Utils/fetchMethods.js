f3550904529368703cf30bf6541f51ad276d63e6
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-05-30 12:42:49 +0200

    Add postStudent method with axios.post

--- frontend/src/Utils/fetchMethods.js
+++ frontend/src/Utils/fetchMethods.js
@@ -11,4 +11,15 @@ export const fetchMessage = async () => {
         console.error("Error fetching message: ", error); 
         throw error; 
     }
-}
+};
+
+export const postStudent = async (student) => {
+    try {
+        await axios.post("http://localhost:8080/students", student);
+        console.log(student); 
+
+    }
+    catch (error) {
+        console.error("Error message: ", error);
+    }
+};
