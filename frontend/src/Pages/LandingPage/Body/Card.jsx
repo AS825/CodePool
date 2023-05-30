b127d7ae1b6f549ab189041f8daa27c4e47e106d
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-30 23:57:52 +0200

    Create Card component

---
+++ frontend/src/Pages/LandingPage/Body/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../LandingPage.css";
+
+function Card({ officePersonal }) {
+  return (
+    <div className="business-container-card">
+      <img
+        src={`data:image/jpeg;base64,${officePersonal.imageSource}`}
+        alt=""
+      />
+      <div className="business-card-text">
+        <h2>
+          {officePersonal.firstName} {officePersonal.lastName}
+        </h2>
+        <p>{officePersonal.jobDescription}</p>
+        <p>{officePersonal.contactData}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
