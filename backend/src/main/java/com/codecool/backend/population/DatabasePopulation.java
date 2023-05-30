d77f6a44cc5e1c10fad71ad4798d4be612e33ced
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-30 14:12:39 +0200

    Refactor populate method

--- backend/src/main/java/com/codecool/backend/population/DatabasePopulation.java
+++ backend/src/main/java/com/codecool/backend/population/DatabasePopulation.java
@@ -1,9 +1,15 @@
 package com.codecool.backend.population;
 
-import com.codecool.backend.model.entity.Company;
-import com.codecool.backend.model.entity.Student;
-import com.codecool.backend.repository.CompanyRepository;
-import com.codecool.backend.repository.StudentRepository;
+import com.codecool.backend.io.FileReader;
+import com.codecool.backend.model.entity.Graduate;
+import com.codecool.backend.persistence.entity.Company;
+import com.codecool.backend.persistence.entity.Student;
+import com.codecool.backend.persistence.repository.GraduateRepository;
+import com.codecool.backend.reader.*;
+import com.codecool.backend.persistence.repository.CompanyRepository;
+import com.codecool.backend.persistence.repository.StudentRepository;
+
+import com.codecool.backend.service.converter.Base64Converter;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,37 +20,27 @@ import java.util.List;
 public class DatabasePopulation {
 
     @Bean
-    ApplicationRunner populate(StudentRepository studentRepository, CompanyRepository companyRepository) {
+    ApplicationRunner populate(StudentRepository studentRepository, CompanyRepository companyRepository, GraduateRepository graduateRepository) {
         return args -> {
             String companyPath = "src/main/resources/csvFiles/Companies-Mock.csv";
             String studentsPath = "src/main/resources/csvFiles/Students-Mock.csv";
+            String graduatesPath="src/main/resources/csvFiles/Graduates.csv";
             FileReader fileReader = new FileReader();
             DataReader dataReader = new DataReader(fileReader);
             StudentTransformer studentTransformer = new StudentTransformer();
             CompanyTransformer companyTransformer = new CompanyTransformer();
+            OfficePersonalTransormer officePersonalTransformer = new OfficePersonalTransformer();
+            Base64Converter base64Converter = new Base64Converter();
+            GraduateTransformer graduateTransformer = new GraduateTransformer(base64Converter);
             List<Student> students = dataReader.read(studentsPath, studentTransformer);
             List<Company> companies = dataReader.read(companyPath, companyTransformer);
+            List<Graduate> graduates = dataReader.read(graduatesPath,graduateTransformer);
 
             companyRepository.saveAll(companies);
             studentRepository.saveAll(students);
+            graduateRepository.saveAll(graduates);
+
         };
-    }
-
-    private List<OfficePersonal> createOfficePersonal() throws IOException {
-        Base64Converter base64Converter = new Base64Converter();
-        return List.of(
-                new OfficePersonal("Sigrid","Hantusch-Taferner","CountryManager","1234", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Sigrid.png")),
-                new OfficePersonal("Lydia","Jeschko","Buisniss Development Austria","fake@email.com", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Lydia.png")),
-                new OfficePersonal("Isabella Kristina","Miglinci","Marketing & Press","fake@email.com", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Isabella.png"))
-        );
-    }
-
-    private List<Graduate> createGraduates() throws IOException {
-        Base64Converter base64Converter = new Base64Converter();
-        return List.of(
-                new Graduate("Mark", "Hartleib", "Schrack Technik", "Junior ERP Developer", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Mark.png")),
-                new Graduate("Roman", "Fann", "Smatrics", "Junior Developer", base64Converter.convertImgToBase64String("/home/bernadette/CodeCool-projects/Modul4/CodePool/backend/img/Roman.png"))
-        );
     }
 
 }
