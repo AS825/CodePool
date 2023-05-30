9faf7f682bbbc1d1caa05c360080cdd4eb15d215
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-30 23:59:18 +0200

    Refactor Footer component

--- frontend/src/Pages/LandingPage/Footer/Footer.jsx
+++ frontend/src/Pages/LandingPage/Footer/Footer.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import Contact from "./Contact";
-import "./Footer.css";
+import Logo from "../../../assets/codeCool_Logo.png";
 
-function Footer({ officePersonal }) {
+function Footer2() {
   return (
-    <div className="footer">
-      <h2>Work with us</h2>
-      {officePersonal == undefined ? (
-        console.log("loading ...")
-      ) : (
-        <Contact officePersonal={officePersonal} />
-      )}
+    <div className="landing-footer">
+      <img src={Logo} alt="" />
+      <div className="contact-div">
+        <h1>CodeCool Austria – Vienna</h1>
+        <h2>Liechtensteinstraße 111-1151090, Wien</h2>
+        <h2>+43 676 557 4252</h2>
+        <h2>business@codecool.com</h2>
+      </div>
     </div>
   );
 }
 
-export default Footer;
+export default Footer2;
