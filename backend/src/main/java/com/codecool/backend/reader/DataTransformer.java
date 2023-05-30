613320fa4067f600cc1f0fe49abce4752bd6b85b
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-30 14:13:03 +0200

    Add IO Exception

--- backend/src/main/java/com/codecool/backend/reader/DataTransformer.java
+++ backend/src/main/java/com/codecool/backend/reader/DataTransformer.java
@@ -1,5 +1,7 @@
 package com.codecool.backend.reader;
 
+import java.io.IOException;
+
 public interface DataTransformer<E> {
-    E transform(String line);
+    E transform(String line) throws IOException;
 }
