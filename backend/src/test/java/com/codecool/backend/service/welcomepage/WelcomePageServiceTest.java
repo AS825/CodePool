066134608e65b7297c83e0364ef6610c260a2092
Alen Sacipi <alensacipi2@gmail.com>
2023-05-30 15:17:21 +0200

    Import graduate & officePersonal repository into Test

--- backend/src/test/java/com/codecool/backend/service/welcomepage/WelcomePageServiceTest.java
+++ backend/src/test/java/com/codecool/backend/service/welcomepage/WelcomePageServiceTest.java
@@ -1,5 +1,6 @@
 package com.codecool.backend.service.welcomepage;
 
+import com.codecool.backend.persistence.repository.GraduateRepository;
 import com.codecool.backend.repository.OfficePersonalRepository;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.mock;
@@ -7,14 +8,15 @@ import static org.mockito.Mockito.*;
 
 
 class WelcomePageServiceTest {
-    com.codecool.backend.repository.GraduateRepository welcomePageRepository = mock(com.codecool.backend.repository.GraduateRepository.class);
+
+   GraduateRepository graduateRepository = mock(GraduateRepository.class);
     OfficePersonalRepository officePersonalRepository = mock(OfficePersonalRepository.class);
-    WelcomePageService welcomePageService = new WelcomePageService(welcomePageRepository, officePersonalRepository);
+    WelcomePageService welcomePageService = new WelcomePageService(graduateRepository, officePersonalRepository);
 
     @Test
     void findAllGraduates() {
         welcomePageService.getGraduatesForSlideshow();
-        verify(welcomePageRepository).findAll();
+        verify(graduateRepository).findAll();
     }
 
     @Test
