d514ec0b98cf571771fe6de174cba42e1c806819
Bernadette Christiner <b.christiner93@gmail.com>
2023-05-30 14:15:00 +0200

    Create OfficePersonalTransformer class

---
+++ backend/src/main/java/com/codecool/backend/reader/OfficePersonalTransformer.java
@@ -0,0 +1,27 @@
+package com.codecool.backend.reader;
+
+import com.codecool.backend.model.entity.Graduate;
+import com.codecool.backend.service.converter.Base64Converter;
+
+import java.io.IOException;
+
+public class OfficePersonalTransformer implements DataTransformer<Graduate> {
+
+        private final Base64Converter base64Converter;
+
+        public OfficePersonalTransformer(Base64Converter base64Converter) {
+            this.base64Converter = base64Converter;
+    }
+
+    @Override
+    public Graduate transform(String line) throws IOException {
+            String[] columns = line.split(",");
+            String firstName = columns[0];
+            String lastName = columns[1];
+            String position = columns[2];
+            String email = columns[3];
+            String image = base64Converter.convertImgToBase64String(columns[4]);
+            return new Graduate(firstName, lastName, position, email, image);
+
+    }
+}
