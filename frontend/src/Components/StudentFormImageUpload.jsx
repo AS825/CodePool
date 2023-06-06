f2b555229bf963dbbef6473453965f551e1971d8
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-06 17:48:55 +0200

    Add StudentFormImageUpload component

---
+++ frontend/src/Components/StudentFormImageUpload.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const StudentFormImageUpload = ({
+  previous,
+  next,
+  uploadedImage,
+  onFileUpload,
+}) => {
+  return (
+    <div className="content-1C">
+      <div className="image-heading">
+        Please upload your image with the transparent background here:
+      </div>
+      <div className="image-section">
+        {uploadedImage ? (
+          <img src={uploadedImage} className="image" />
+        ) : (
+          <img src="/person.png" className="image" />
+        )}
+        <label htmlFor="image-input" className="image-label">
+          Upload Image
+          <input
+            id="image-input"
+            name="image"
+            type="file"
+            className="image-input"
+            onChange={(event) => onFileUpload(event)}
+          />
+        </label>
+      </div>
+      <div className="buttons">
+        <button className="button" onClick={() => previous()}>
+          Previous
+        </button>
+        <button className="button" onClick={() => next()}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default StudentFormImageUpload;
