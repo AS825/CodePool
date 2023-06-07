fb9ee4172b9d1924b9f59e3f0fdb0a92e8622edb
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-07 08:08:52 +0200

    Adapt StudentFormImageUpload component

--- frontend/src/Components/StudentFormImageUpload.jsx
+++ frontend/src/Components/StudentFormImageUpload.jsx
@@ -7,26 +7,28 @@ const StudentFormImageUpload = ({
   onFileUpload,
 }) => {
   return (
-    <div className="content-1C">
-      <div className="image-heading">
+    <div className="form">
+      <div className="form-heading">
         Please upload your image with the transparent background here:
       </div>
-      <div className="image-section">
-        {uploadedImage ? (
-          <img src={uploadedImage} className="image" />
-        ) : (
-          <img src="/person.png" className="image" />
-        )}
-        <label htmlFor="image-input" className="image-label">
-          Upload Image
-          <input
-            id="image-input"
-            name="image"
-            type="file"
-            className="image-input"
-            onChange={(event) => onFileUpload(event)}
-          />
-        </label>
+      <div className="content-1C">
+        <div className="image-section">
+          {uploadedImage ? (
+            <img src={uploadedImage} className="image" />
+          ) : (
+            <img src="/person.png" className="image" />
+          )}
+          <label htmlFor="image-input" className="image-label">
+            Upload Image
+            <input
+              id="image-input"
+              name="image"
+              type="file"
+              className="image-input"
+              onChange={(event) => onFileUpload(event)}
+            />
+          </label>
+        </div>
       </div>
       <div className="buttons">
         <button className="button" onClick={() => previous()}>
