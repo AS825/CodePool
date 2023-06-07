ef8691c0c424d767ebc0bd5755fc9c654740903a
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-07 08:31:00 +0200

    Adapt StudentFormIntro component

--- frontend/src/Components/StudentFormIntro.jsx
+++ frontend/src/Components/StudentFormIntro.jsx
@@ -4,8 +4,17 @@ const StudentFormIntro = ({ next }) => {
   return (
     <div className="form">
       <div className="form-heading">
-        Description on how to remove background
+        Background-Removal of your profile image
       </div>
+      <div className="form-text">
+        For your profile image it is necessary to remove the background of the
+        image. You can do that on{" "}
+        <a href="https://www.remove.bg/de">this webpage</a>:
+      </div>
+      <div className="image-backgroundRemoval-container">
+        <img src="/backgroundRemoval.png" className="image-backgroundRemoval" />
+      </div>
+
       <div className="content-1C"></div>
       <div className="buttons">
         <button className="button" onClick={() => next()}>
