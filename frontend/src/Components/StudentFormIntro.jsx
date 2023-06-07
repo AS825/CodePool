2461b390fdae6616b9cf551f97d52e7b81db6015
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-07 08:09:53 +0200

    Adapt StudentFormIntro component

--- frontend/src/Components/StudentFormIntro.jsx
+++ frontend/src/Components/StudentFormIntro.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const StudentFormIntro = ({next}) => {
+const StudentFormIntro = ({ next }) => {
   return (
-    <div className="content-1C">
-          StudentFormIntro
-          <div className="buttons">
-            <button className="button" onClick={() => next()}>
-              Next
-            </button>
-          </div>
+    <div className="form">
+      <div className="form-heading">
+        Description on how to remove background
+      </div>
+      <div className="content-1C"></div>
+      <div className="buttons">
+        <button className="button" onClick={() => next()}>
+          Next
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StudentFormIntro
+export default StudentFormIntro;
