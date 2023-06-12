89c3957ff17dea72cd2a031567aabc69e833b56d
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-12 22:34:13 +0200

    Adapt StudentCreator

--- frontend/src/Pages/StudentCreatorPage/StudentCreator.jsx
+++ frontend/src/Pages/StudentCreatorPage/StudentCreator.jsx
@@ -1,18 +1,15 @@
-import {useState} from 'react'; 
 import { useNavigate } from 'react-router-dom';
 import { postStudent } from '../../Utils/fetchMethods';
 import StudentForm from './StudentForm/StudentForm';
 
 const StudentCreator = () => {
   const navigate = useNavigate(); 
-  const [loading, setLoading] = useState(false); 
 
   const createStudent = (student) => {
-    setLoading(true); 
     postStudent(student)
       .then(() => {
-        navigate("/");
-        setLoading(false); 
+        navigate("/students/create");
+        console.log("student: ", student); 
       })
       .catch(error => {
         throw error; 
@@ -27,7 +24,6 @@ const StudentCreator = () => {
     <StudentForm
       onSubmit={createStudent}
       onCancel={performCancelling}
-      disabled={loading}
     />
   );
 };
