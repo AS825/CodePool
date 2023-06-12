3eab4af93db9d7ab6ec83c75d6629adad4232358
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-12 22:34:54 +0200

    Add getTechnologies to fetchMethods

--- frontend/src/Utils/fetchMethods.js
+++ frontend/src/Utils/fetchMethods.js
@@ -1,33 +1,31 @@
 import axios from "axios";
 
-const path = "http://localhost:8080/"; 
+const path = "http://localhost:8080/";
 
 export const fetchMessage = async () => {
-    try {
-        const response = await axios.get(path); 
-        return response.data; 
-    }
-    catch (error) {
-        console.error("Error fetching message: ", error); 
-        throw error; 
-    }
+  try {
+    const response = await axios.get(path);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching message: ", error);
+    throw error;
+  }
 };
 
 export const postStudent = async (student) => {
-    try {
-        await axios.post("http://localhost:8080/students", student);
-
-    }
-    catch (error) {
-        console.error("Error message: ", error);
-    }
+  try {
+    await axios.post("http://localhost:8080/students", student);
+  } catch (error) {
+    console.error("Error message: ", error);
+  }
 };
 
-export const postImage = async (image) => {
-    try {
-        await axios.post("http://localhost:8080/students", student);
-    }
-    catch (error) {
-        console.error("Error message: ", error);
-    }
+export const getTechnologies = async () => {
+  try {
+    const response = await axios.get("http://localhost:8080/technologies");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching message: ", error);
+    throw error;
+  }
 };
