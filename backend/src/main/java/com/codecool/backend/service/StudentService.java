959ed09de124d3287e6bd2ab846e78cfaf298def
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-13 12:38:57 +0200

    Adapt save method in StudentService to save technologies

--- backend/src/main/java/com/codecool/backend/service/StudentService.java
+++ backend/src/main/java/com/codecool/backend/service/StudentService.java
@@ -1,20 +1,25 @@
 package com.codecool.backend.service;
 
-import com.codecool.backend.api.exception.ElementNotFoundException;
 import com.codecool.backend.persistence.entity.Student;
+import com.codecool.backend.persistence.entity.Technology;
 import com.codecool.backend.persistence.repository.StudentRepository;
+import com.codecool.backend.persistence.repository.TechnologyRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
 
     private final StudentRepository studentRepository;
+    private final TechnologyRepository technologyRepository;
 
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository, TechnologyRepository technologyRepository) {
         this.studentRepository = studentRepository;
+        this.technologyRepository = technologyRepository;
     }
 
     public List<Student> findAll() {
@@ -26,6 +31,16 @@ public class StudentService {
     }
 
     public Student save(Student student) {
+        List<Technology> technologies = student.getTechnologies();
+        List<Technology> allDistinctTechnologies = technologyRepository.findAll()
+                .stream()
+                .collect(Collectors.toMap(Technology::getImage, Function.identity(), (t1, t2) -> t1))
+                .values()
+                .stream().toList();
+        List<Technology> persistedTechnologies = technologies.stream()
+                .map(technology -> allDistinctTechnologies.stream().filter(tech -> tech.getId() == technology.getId()).findFirst().get())
+                .collect(Collectors.toList());
+        student.setTechnologies(persistedTechnologies);
         return studentRepository.save(student);
     }
 
