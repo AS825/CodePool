2d261dee8aba0b78ced9fe8dd4f689c3cb9a7cf0
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-13 12:35:43 +0200

    Expand fields in Student class

--- backend/src/main/java/com/codecool/backend/persistence/entity/Student.java
+++ backend/src/main/java/com/codecool/backend/persistence/entity/Student.java
@@ -2,24 +2,35 @@ package com.codecool.backend.persistence.entity;
 
 import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.Objects;
 
-
 @Entity
-public final class Student {
+public class Student {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
-    private String description;
-    private String project;
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String email;
+    private String selfDescription;
+    private String projectDescription;
+    @Column(length = 1000000)
     private String image;
 
-    public Student(String name, String description, String project, String image) {
-        this.name = name;
-        this.description = description;
-        this.project = project;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Technology> technologies;
+
+    public Student(String firstName, String lastName, String age, String email, String selfDescription, String projectDescription, String image, List<Technology> technologies) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.selfDescription = selfDescription;
+        this.projectDescription = projectDescription;
         this.image = image;
+        this.technologies = technologies;
     }
 
     public Student() {
@@ -33,28 +44,52 @@ public final class Student {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getDescription() {
-        return description;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public String getProject() {
-        return project;
+    public String getAge() {
+        return age;
     }
 
-    public void setProject(String project) {
-        this.project = project;
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSelfDescription() {
+        return selfDescription;
+    }
+
+    public void setSelfDescription(String selfDescription) {
+        this.selfDescription = selfDescription;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
     }
 
     public String getImage() {
@@ -65,17 +100,24 @@ public final class Student {
         this.image = image;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, project, image);
+    public List<Technology> getTechnologies() {
+        return technologies;
+    }
+
+    public void setTechnologies(List<Technology> technology) {
+        this.technologies = technology;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Student student = (Student) obj;
-        return id == student.id && Objects.equals(name, student.name) && Objects.equals(description, student.description)
-               && Objects.equals(project, student.project) && Objects.equals(image, student.image);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(age, student.age) && Objects.equals(email, student.email) && Objects.equals(selfDescription, student.selfDescription) && Objects.equals(projectDescription, student.projectDescription) && Objects.equals(image, student.image) && Objects.equals(technologies, student.technologies);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, age, email, selfDescription, projectDescription, image, technologies);
     }
 }
