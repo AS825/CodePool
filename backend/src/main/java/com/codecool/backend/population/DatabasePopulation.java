b249a8e6a14cceaabe1de9d757941d6da8dff79d
PeterNiederwieser <peter.niederwieser@gmail.com>
2023-06-13 12:24:27 +0200

    Expand DatabasePopulation for technologies

--- backend/src/main/java/com/codecool/backend/population/DatabasePopulation.java
+++ backend/src/main/java/com/codecool/backend/population/DatabasePopulation.java
@@ -5,10 +5,9 @@ import com.codecool.backend.model.entity.Graduate;
 import com.codecool.backend.model.entity.OfficePersonal;
 import com.codecool.backend.persistence.entity.Company;
 import com.codecool.backend.persistence.entity.Student;
-import com.codecool.backend.persistence.repository.GraduateRepository;
+import com.codecool.backend.persistence.entity.Technology;
+import com.codecool.backend.persistence.repository.*;
 import com.codecool.backend.reader.*;
-import com.codecool.backend.persistence.repository.CompanyRepository;
-import com.codecool.backend.persistence.repository.StudentRepository;
 
 import com.codecool.backend.service.converter.Base64Converter;
 import org.springframework.boot.ApplicationRunner;
@@ -25,29 +24,33 @@ public class DatabasePopulation {
             StudentRepository studentRepository,
             CompanyRepository companyRepository,
             GraduateRepository graduateRepository,
-            com.codecool.backend.repository.OfficePersonalRepository officePersonalRepository) {
+            OfficePersonalRepository officePersonalRepository,
+            TechnologyRepository technologyRepository) {
         return args -> {
             String companyPath = "src/main/resources/csvFiles/Companies-Mock.csv";
             String studentsPath = "src/main/resources/csvFiles/Students-Mock.csv";
             String graduatesPath = "src/main/resources/csvFiles/Graduates.csv";
             String officePersonalPath = "src/main/resources/csvFiles/OfficePersonal.csv";
+            String technologiesPath = "src/main/resources/csvFiles/Technologies.csv";
             FileReader fileReader = new FileReader();
             DataReader dataReader = new DataReader(fileReader);
-            StudentTransformer studentTransformer = new StudentTransformer();
-            CompanyTransformer companyTransformer = new CompanyTransformer();
             Base64Converter base64Converter = new Base64Converter();
+            StudentTransformer studentTransformer = new StudentTransformer(base64Converter);
+            CompanyTransformer companyTransformer = new CompanyTransformer();
             OfficePersonalTransformer officePersonalTransformer = new OfficePersonalTransformer(base64Converter);
             GraduateTransformer graduateTransformer = new GraduateTransformer(base64Converter);
+            TechnologyTransformer technologyTransformer = new TechnologyTransformer(base64Converter);
             List<Student> students = dataReader.read(studentsPath, studentTransformer);
             List<Company> companies = dataReader.read(companyPath, companyTransformer);
             List<Graduate> graduates = dataReader.read(graduatesPath, graduateTransformer);
             List<OfficePersonal> officePersonal = dataReader.read(officePersonalPath, officePersonalTransformer);
+            List<Technology> technologies = dataReader.read(technologiesPath, technologyTransformer);
 
+            technologyRepository.saveAll(technologies);
             companyRepository.saveAll(companies);
             studentRepository.saveAll(students);
             graduateRepository.saveAll(graduates);
             officePersonalRepository.saveAll(officePersonal);
         };
     }
-
 }
