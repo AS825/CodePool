255b1cb08b0d605a9c012e04268af61b95c1cd98
Alen Sacipi <alensacipi2@gmail.com>
2023-06-14 12:42:28 +0200

    Add Spring-security configuration yml

--- backend/src/main/java/com/codecool/backend/security/auth/service/AuthenticationService.java
+++ backend/src/main/java/com/codecool/backend/security/auth/service/AuthenticationService.java
@@ -1,14 +1,17 @@
 package com.codecool.backend.security.auth.service;
 
+import com.codecool.backend.security.auth.data.AuthenticationRequest;
 import com.codecool.backend.security.auth.data.AuthenticationResponse;
 import com.codecool.backend.security.auth.data.RegisterRequest;
 import com.codecool.backend.security.configuration.JwtService;
+import com.codecool.backend.security.entity.user.Role;
 import com.codecool.backend.security.entity.user.User;
 import com.codecool.backend.security.entity.user.UserRepository;
 import com.codecool.backend.security.token.Token;
 import com.codecool.backend.security.token.TokenRepository;
 import com.codecool.backend.security.token.TokenType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,18 +39,48 @@ public class AuthenticationService {
         UserDetails user = new User(
                 request.getUsername(),
                 request.getEmail(),
-                request.getPassword(),
-                request.getRole()
+                passwordEncoder.encode(request.getPassword()),
+                request.getRole() == null ? Role.USER : request.getRole()
         );
 
         UserDetails savedUser = userRepository.save(user);
         String jwtToken = jwtService.generateToken(user);
-        String refreshToken = jwtService.generateRefreshToken(user);
         saveUserToken(savedUser, jwtToken);
         return new AuthenticationResponse(
                 ((User) user).getName(),
                 jwtToken
         );
+    }
+
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens((User) user);
+        saveUserToken(user, jwtToken);
+        return new AuthenticationResponse(
+                ((User) user).getName(),
+                jwtToken
+        );
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
     }
 
     private void saveUserToken(UserDetails user, String jwtToken) {
@@ -60,4 +93,5 @@ public class AuthenticationService {
         );
         tokenRepository.save(token);
     }
+
 }
