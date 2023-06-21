82f1100e430e7e231f4459de6613ca29d46fd5fb
Bernadette Christiner <b.christiner93@gmail.com>
2023-06-21 10:51:33 +0200

    Remove import statement

--- backend/src/main/java/com/codecool/backend/security/entity/user/User.java
+++ backend/src/main/java/com/codecool/backend/security/entity/user/User.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
@@ -20,7 +21,6 @@ public class User implements UserDetails {
     @Column(unique = true)
     private String email;
     private String password;
-
     @Enumerated(EnumType.STRING)
     private Role role;
 
